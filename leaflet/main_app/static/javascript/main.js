a5bc8a32b68e3842f72fd8768e6ff65dcc37880e
Bonny R <bonnyrivers@gmail.com>
2019-03-15 12:35:35 -0700

    update pages and add css materialize

---
+++ leaflet/main_app/static/javascript/main.js
@@ -0,0 +1,7 @@
+// For collapsing side nav
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+  
