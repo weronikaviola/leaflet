366a6c07066d9150a63afd7947ede7724e89b316
Weronika Miller <weronika.altowka@gmail.com>
2019-03-15 13:18:35 -0700

    new

--- leaflet/main_app/forms.py
+++ leaflet/main_app/forms.py
@@ -6,4 +6,4 @@ class UserForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['username', 'password', 'email', 'first_name']
