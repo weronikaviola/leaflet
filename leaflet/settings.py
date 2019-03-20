5b04764cdf65f1badd14eead7ee4a8fdee4d5263
Weronika Miller <weronika.altowka@gmail.com>
2019-03-20 13:12:11 -0700

    changes'

--- leaflet/settings.py
+++ leaflet/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 LOGIN_REDIRECT_URL = '/main/'
 
 LOGOUT_REDIRECT_URL = '/'
