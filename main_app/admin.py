0a0e4939a464ffc51d6852b53b5f08b648ac423d
Weronika Miller <weronika.altowka@gmail.com>
2019-03-22 09:12:08 -0700

    admin updated

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Profile, Photo, Event
+from .models import Profile, Photo, Event, Posting, Alert
 
 
 admin.site.register(Profile)
 admin.site.register(Event)
 admin.site.register(Photo)
+admin.site.register(Posting)
+admin.site.register(Alert)
